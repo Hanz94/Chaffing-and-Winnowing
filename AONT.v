b5a472fe85d7084eed598d6198876016b296f6c3
Hansika <hansikam94@gmail.com>
2021-04-24 16:51:43 -0400

    replace counter in int with specific boolean val

--- AONT.v
+++ AONT.v
@@ -42,62 +42,62 @@ module latin_square#(
       parameter lslenlog = 4
 	)
     (  input clk,
-       input wire[0:lslen-1][lslenlog-1:0] firstRow ,   
-       output reg[0:lslen-1][0:lslen-1][lslenlog-1:0] latinsquare 
+       input wire[lslen-1:0][lslenlog-1:0] firstRow ,   
+       output reg[lslen-1:0][lslen-1:0][lslenlog-1:0] latinsquare 
     ); 
 
-    integer j = 0 ;
+    integer j;
 
     always @ (posedge clk) begin
         latinsquare[0] = firstRow;
 
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[1][j] = ( 1 * latinsquare[1][j] ) % lslen;
+            latinsquare[1][j] = ( 4'b0001 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[2][j] = ( 2 * latinsquare[1][j] ) % lslen;
+            latinsquare[2][j] = ( 4'b0010 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[3][j] = ( 3 * latinsquare[1][j] ) % lslen;
+            latinsquare[3][j] = ( 4'b0011  * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[4][j] = ( 4 * latinsquare[1][j] ) % lslen;
-        end
-
-        for (j=0; j<lslen; j=j+1) begin
-            latinsquare[5][j] = ( 5 * latinsquare[1][j] ) % lslen;
-        end
-        for (j=0; j<lslen; j=j+1) begin
-            latinsquare[6][j] = ( 6 * latinsquare[1][j] ) % lslen;
-        end
-        for (j=0; j<lslen; j=j+1) begin
-            latinsquare[7][j] = ( 7 * latinsquare[1][j] ) % lslen;
-        end
-        for (j=0; j<lslen; j=j+1) begin
-            latinsquare[8][j] = ( 8 * latinsquare[1][j] ) % lslen;
+            latinsquare[4][j] = ( 4'b0100 * latinsquare[1][j] ) % lslen;
         end
 
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[9][j] = ( 9 * latinsquare[1][j] ) % lslen;
+            latinsquare[5][j] = ( 4'b0101 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[10][j] = ( 10 * latinsquare[1][j] ) % lslen;
+            latinsquare[6][j] = ( 4'b0110 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[11][j] = ( 11 * latinsquare[1][j] ) % lslen;
+            latinsquare[7][j] = ( 4'b0111 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[12][j] = ( 12 * latinsquare[1][j] ) % lslen;
+            latinsquare[8][j] = ( 4'b1000 * latinsquare[1][j] ) % lslen;
         end
 
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[13][j] = ( 13 * latinsquare[1][j] ) % lslen;
+            latinsquare[9][j] = ( 4'b1001 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[14][j] = ( 14 * latinsquare[1][j] ) % lslen;
+            latinsquare[10][j] = ( 4'b1010 * latinsquare[1][j] ) % lslen;
         end
         for (j=0; j<lslen; j=j+1) begin
-            latinsquare[15][j] = ( 15 * latinsquare[1][j] ) % lslen;
+            latinsquare[11][j] = ( 4'b1011 * latinsquare[1][j] ) % lslen;
+        end
+        for (j=0; j<lslen; j=j+1) begin
+            latinsquare[12][j] = ( 4'b1100 * latinsquare[1][j] ) % lslen;
+        end
+
+        for (j=0; j<lslen; j=j+1) begin
+            latinsquare[13][j] = ( 4'b1101 * latinsquare[1][j] ) % lslen;
+        end
+        for (j=0; j<lslen; j=j+1) begin
+            latinsquare[14][j] = ( 4'b1110 * latinsquare[1][j] ) % lslen;
+        end
+        for (j=0; j<lslen; j=j+1) begin
+            latinsquare[15][j] = ( 4'b1111 * latinsquare[1][j] ) % lslen;
         end
     end
 
