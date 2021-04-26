79dfa871dea59c801bdac5429b5c2e832a0aaa30
Hansika <hansikam94@gmail.com>
2021-04-25 22:09:47 -0400

    cw done

--- cw.sv
+++ cw.sv
@@ -6,19 +6,53 @@ module cw #(
 	   parameter cwbits = 32,
      parameter ctrsize = 16,
      parameter tagsize = 16,
-     parameter cachesize = 10
+     parameter cachesize = 64
 	)
-    (  input clk,               
+  (  input clk,               
        input rstn,
-       output reg[ctrsize+tagsize: 0] out [0:cwbits*2-1]);   
+       input wire [ctrsize-1:0] ctr,
+       input wire [cwbits-1:0] msgIn,
+       input wire[cachesize-1:0][tagsize -1:0] maccache,
+       output reg[cwbits*2-1:0][ctrsize+tagsize: 0] msgOut
+  );   
   
-  reg[tagsize -1 : 0] maccache [0:cachesize];
 
   integer j = 0 ;
+  reg[cachesize-1:0][tagsize -1:0] macCacheReg;
+
   always @ (posedge clk) begin
+    macCacheReg = maccache;
+
     for (j=0; j<cwbits; j++) begin
-        out[j*2][1] =  maccache[1][1];
-        out[j*2+1][1] =  maccache[1][1];
+        msgOut[j*2] =  { msgIn[j], macCacheReg[j], ctr};
+        msgOut[j*2+1] =  { ~msgIn[j], macCacheReg[cwbits -1 - j], ctr};
     end
   end
+
 endmodule
+
+
+
+
+
+
+// module cwMini #(
+// 	   parameter cwbits = 32,
+//      parameter ctrsize = 16,
+//      parameter tagsize = 16,
+//      parameter cachesize = 64
+// 	)
+//   (  input clk,               
+//        input wire[1:0][1:0] rstn,
+//        input wire [1:0]ctr,
+//        output reg[1:0][2:0] msgOut
+//   );   
+  
+//   integer j = 0 ;
+//   always @ (posedge clk) begin
+//     for (j=0; j<cwbits; j++) begin
+//       msgOut[j] =  { ctr[j], rstn[j]};
+//     end
+//   end
+  
+// endmodule
